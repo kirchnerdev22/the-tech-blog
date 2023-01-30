15ca67cc512db60a61780817cd641d168b2ebd16
kirchnerdev22 <trentonlkirchner@gmail.com>
2023-01-30 12:25:46 -0600

    filling in the controllers and models

---
+++ controllers/api/userRoutes.js
@@ -0,0 +1,69 @@
+const router = require('express').Router();
+const { User } = require('../../models');
+
+//Create a new user
+router.post("/", async (req, res) => {
+try {
+  const userData = await User.create(req.body);
+  req.session.save(() => {
+    req.session.logged_in = true;
+    req.session.user_id = userData.id;
+    res.status(200).json(userData);
+  });
+} catch (err) {
+  res.status(500).json(err)
+}
+});
+
+// Login User
+router.post('/session', async (req, res) => {
+  try {
+    // Find the user who matches the posted e-mail address
+    const userData = await User.findOne({ where: { displayName: req.body.display_name } });
+
+    if (!userData) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect email or password, please try again' });
+      return;
+    }
+
+    // Verify the posted password with the password store in the database
+    const validPassword = await userData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect email or password, please try again' });
+      return;
+    }
+
+    // Create session variables based on the logged in user
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+      
+      res.json({ user: userData, message: 'You are now logged in!' });
+    });
+
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.post('/logout', (req, res) => {
+ try {
+   if (req.session.logged_in) {
+      // Remove the session variables
+      req.session.destroy(() => {
+        res.status(204).end();
+      });
+    } else {
+      res.status(404).end();
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
---
+++ models/index.js
@@ -0,0 +1,8 @@
+const User = require('./User');
+const Comments = require('./Comments');
+const Post = require('./Post');
+
+User.hasMany(Post, {as:"posts"});
+Post.belongsTo(User, {as:"user"});
+
+module.exports = { User, Comments, Post };
